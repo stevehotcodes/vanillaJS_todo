c28554a8f7e0ef1aa387a888a949a22c234e1507
Stephen Ondieki <stevehotcodes@gmail.com>
2023-10-16 16:17:01 +0300

    removed comments for the delete button

--- index.js
+++ index.js
@@ -21,11 +21,18 @@ window.addEventListener('load',()=>{
         // tasks.classList.add("tasks");
         // console.log(tasks)
 
-
+        const checkMark=document.createElement("div");
+        checkMark.style.width="20px";
+        checkMark.style.height="20px";
+        checkMark.style.background="red";
+        checkMark.style.opacity="0"
+        checkMark.style.margin="1rem";
         const task_el=document.createElement("div");
         task_el.classList.add("task");
         // tasks.appendChild(task_el);
         
+       
+
         const task_content_el=document.createElement("input");
         task_content_el.type="text"
         task_content_el.value=task
@@ -45,7 +52,8 @@ window.addEventListener('load',()=>{
         
         task_content_el.setAttribute('readonly','readonly');
         task_content_el.classList.add("task-content")
-        task_el.appendChild(task_content_el)
+        task_el.appendChild(task_content_el);
+        task_el.appendChild(checkMark)
 
         const task_actions_el=document.createElement("div");
         task_actions_el.classList.add("task-actions");
@@ -75,6 +83,7 @@ window.addEventListener('load',()=>{
                 task_content_el.style.backgroundColor="pink"
                 edit_btn.innerHTML="Save"
                 edit_btn.style.color="blue"
+            
                 
                 
             }
@@ -89,8 +98,12 @@ window.addEventListener('load',()=>{
         })
 
         delete_btn.addEventListener('click',()=>{
-            list_el.removeChild(task_el)
-        console.log(task_content_el);
+            // list_el.removeChild(task_el)
+
+            task_content_el.style.backgroundColor="red";
+            task_content_el.style.textDecoration="line-through"
+            delete_btn.innerHTML=""
+            edit_btn.style.display="none"
         })   
 
         
